852e98ea241c837d90c82f8c40f13a03a2a9786d
Rafael Cruz <rafac@Rafaels-MacBook-Pro.local>
2018-10-17 14:29:00 -0400

    cosmetic changes

--- src/components/price.js
+++ src/components/price.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-export default class Price extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          currency: 'USD',
-          price: '' 
-      }
+class Price extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currency: "USD",
+      price: ""
+    };
   }
 
   componentDidMount() {
-      this.getBitcoinPrice()
+    this.getBitcoinPrice();
   }
 
-  getBitcoinPrice(){
-      axios.get('https://api.coindesk.com/v1/bpi/currentprice/'+this.state.currency+'.json')
-           .then((response) => {
-               this.setState({ price: response.data.bpi.USD.rate})
-           })
-           .catch((err) => {
-               console.log(err);
-           })
+  getBitcoinPrice() {
+    axios
+      .get(
+        "https://api.coindesk.com/v1/bpi/currentprice/" +
+          this.state.currency +
+          ".json"
+      )
+      .then(response => {
+        this.setState({ price: response.data.bpi.USD.rate });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
+
   render() {
-    return (
-      <div style={{color: 'green'}}>
-        $ {this.state.price}
-      </div>
-    )
+    return <div style={{ color: "green" }}>$ {this.state.price}</div>;
   }
 }
+
+export default Price;
